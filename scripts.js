58d8cb72c92761c7bbd59a731cbf31a75624fdb8
Lakshmi Satharaboina <satharaboinalakshmi@gmail.com>
2024-06-03 07:05:55 +0530

    first commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-  
+const bilAmnt = document.getElementById("bill");
+const tipPercent = document.getElementById("tip");
+const btnEl = document.getElementById("btn");
+const totalEl = document.getElementById("total");
+
+function totalAmountCalculation() {
+	const bilvalue = bilAmnt.value;
+	const tipvalue = tipPercent.value;
+	const totalAmt = bilvalue * (1 + tipvalue / 100);
+	totalEl.innerText = totalAmt.toFixed(0);
+}
+
+btnEl.addEventListener("click", totalAmountCalculation);
